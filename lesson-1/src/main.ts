46dd1b58e8e8527eb2b774e1568cf07a779457d7
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-09 23:42:56 +0100

    Lesson 2: Basic types

--- lesson-1/src/main.ts
+++ lesson-1/src/main.ts
@@ -1,13 +1,27 @@
-let username = 'Abdulmalik';
-let age = 25;
-let isMale = true;
-let hobbies = ['football', 'coding', 'reading'];  
-let address = ['Nigeria', 'Lagos', 'Ikeja'];
+// LESSON 1
+// let username = 'Abdulmalik';
+// let age = 25;
+// let isMale = true;
+// let hobbies = ['football', 'coding', 'reading'];  
+// let address = ['Nigeria', 'Lagos', 'Ikeja'];
 
-console.log(address, hobbies, isMale, age, username);
+// console.log(address, hobbies, isMale, age, username);
 
-let a: number = 10;
-let b: number = 20;
-let c: number = a + b;
+// let a: number = 10;
+// let b: number = 20;
+// let c: number = a + b;
 
-console.log(c);
+// console.log(c);
+
+// LESSON 2: BASIC TYPES
+
+let username: string;
+let meaningOfLife: number;
+let isLoading:boolean
+let album:any
+
+// union type
+let person: string | number
+
+let postId: number | string
+let isActive: boolean | number
