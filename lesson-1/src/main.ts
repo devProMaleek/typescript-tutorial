c9c260a6c1924578e16b86ae4836f7eecda38827
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-10 12:06:16 +0100

    Learnt Function in Typescript

--- lesson-1/src/main.ts
+++ lesson-1/src/main.ts
@@ -15,7 +15,7 @@
 
 // LESSON 2: BASIC TYPES
 
-let username: string;
+// let username: string;
 let meaningOfLife: number;
 let isLoading: boolean;
 let album: any;
@@ -54,15 +54,70 @@ let Eniola = {
   age: 28,
   isAdmin: true,
   gender: 'Female',
-}
+};
 
-Abdulmalik = Eniola
+Abdulmalik = Eniola;
 
 const nameAgeMap: { [index: string]: number } = {};
 const nameBooleanMap: { [index: string]: boolean } = {};
-nameAgeMap.malik = 12
-nameAgeMap.Eniola = 34
-nameAgeMap.damola = 45
+nameAgeMap.malik = 12;
+nameAgeMap.Eniola = 34;
+nameAgeMap.damola = 45;
 
-nameBooleanMap.malik = true
-nameBooleanMap.true = false
+nameBooleanMap.malik = true;
+nameBooleanMap.true = false;
+
+// Literal Types
+
+let username: 'Abdulmalik' | 'Adeola' | 'Adebayo';
+
+username = 'Adeola';
+
+type mathFunction = (x: number, y: number) => number;
+
+interface MathFunction {
+  (x: number, y: number): number;
+}
+
+const multiplyFunction: mathFunction = (x, y) => {
+  return Math.pow(x, y);
+};
+
+// Optional Parameters
+
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c === 'number') {
+    //Type check/guard
+    return a + b + c;
+  }
+  return a + b;
+};
+
+// custom type guard/check
+
+const isNumber = (value: any): boolean => {
+  return typeof value === 'number';
+};
+
+const isString = (value: any): boolean => {
+  return typeof value === 'string';
+};
+
+// never type
+const createError = (message: string): never => {
+  throw new Error(message);
+};
+
+// use of never type
+
+const numberOrString = (value: number | string): string => {
+  if (isNumber(value)) {
+    return 'number';
+  }
+
+  if (isString(value)) {
+    return 'string';
+  }
+
+  return createError('Invalid');
+};
